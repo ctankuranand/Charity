acb7257de390004ede33a565bb5b3e99493a5d1f
ctankuranand <41411271+ctankuranand@users.noreply.github.com>
2019-08-13 02:19:08 +0700

    Add files via upload
    Second commit

---
+++ home.js
@@ -0,0 +1,12 @@
+function myFunction(subject,level) {
+	var homeElement = document.getElementById("home");
+	homeElement.style.display = "none";
+	var topicselect = document.getElementById("topicselection");
+	topicselect.style.display = "block"
+
+
+
+	console.log(subject);
+	console.log(level);
+
+} 
